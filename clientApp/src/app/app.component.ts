ee17e951da6db82b6edac278e79b82c25bdbfffb
amiradahmani <dahmani.am2004@gmail.com>
2024-12-19 11:16:05 +0100

    dashboard presentetion

--- clientApp/src/app/app.component.ts
+++ clientApp/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AuthComponent } from '../auth/presentation/auth.component';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,AuthComponent],
