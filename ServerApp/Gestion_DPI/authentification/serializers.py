e5be134c085bc8d2ebcc8a0ac2053b4c1016d171
Melliti Abdelmalek <ma_melliti@esi.dz>
2024-12-20 23:09:32 +0100

    Application Backend Django avec la première version de l'authentification.

---
+++ ServerApp/Gestion_DPI/authentification/serializers.py
@@ -0,0 +1,5 @@
+from rest_framework import serializers
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
