64760807394ef56cb43163148e628a433feb8565
Melliti Abdelmalek <ma_melliti@esi.dz>
2024-12-23 18:50:34 +0100

    Amélioration de l'authentification et ajout de la documentation. Ajout d'une fonction de création d'un utilisateur et une fonction logout de déconnexion

--- ServerApp/Gestion_DPI/Gestion_DPI/settings.py
+++ ServerApp/Gestion_DPI/Gestion_DPI/settings.py
@@ -38,13 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'authentification',
     'corsheaders',
+    'drf_yasg',
 ]
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    ]
+]
 
 AUTH_USER_MODEL = 'authentification.User'
 
@@ -58,6 +60,7 @@ REST_FRAMEWORK = {
 
 from datetime import timedelta
 SIMPLE_JWT = {
+    'AUTH_HEADER_TYPES': ('Bearer',),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
--- ServerApp/Gestion_DPI/authentification/models.py
+++ ServerApp/Gestion_DPI/authentification/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True, blank=True)
+    email = models.EmailField(unique=False, blank=True)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
